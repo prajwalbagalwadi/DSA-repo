b6b78f6117c1455e83c0e04ac91aa49f4cfcfec2
prajwalbagalwadi <47298268+prajwalbagalwadi@users.noreply.github.com>
2025-12-13 14:31:27 +0530

    Update kadanes_algorithm_array.c

--- kadanes_algorithm_array.c
+++ kadanes_algorithm_array.c
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-/*******************************************************************************/
 /*
 This is kadanens_algorthim for sum of sub array so
 
@@ -62,4 +55,5 @@ int main()
     return 0;
 
 }
+
 
